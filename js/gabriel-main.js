35429745c90058e329996b2e90f235a8e7c4ddd9
developer <developer@glb0005.(none)>
2013-04-24 09:40:41 -0500

    Drag and Drop

--- js/gabriel-main.js
+++ js/gabriel-main.js
@@ -1,41 +1,47 @@
 $.widget("ui.acordion", {
 	  options: {
-	       accordion: true,
+	           accordion: true,
 		   draggable : true,
-		   droppable: true
+		   droppable: true,
+		   element_accordion: ".accordion",
+		   element_draggable: ".draggable",
+		   element_droppable: ".droppable"
 	    },
 	    _create: function() {
-		
-                if(this.options.accordion == true){
-					 $("article").accordion();
-				}
-				
-				
-				 if(this.options.draggable == true){
-
-					  $(".draggable").draggable({					  					
-					  revert: true 					
-					  
-					  });		 
-						
-					 
-				}
-				
-				 if(this.options.droppable == true){
-					   $( "aside" ).droppable({
-					   
-					       
-							  drop: function( event, ui ) {
-							  $('aside').append(ui.draggable);
-							  $(ui.draggable).removeAttr("style").removeClass('draggable');
-										
-						}
-				
-						
-						})
-
-				}
-					    
+	    	this._isAcordion();
+	    	this._isDraggable();
+	    	this._isDroppable();
+		    
+	    },
+	    
+	    _isAcordion: function(){
+	        if(this.options.accordion == true){
+				 $(this.options.element_accordion).accordion();
+			}
+	        return this.options.accordion;
+	    },
+	    
+	    _isDraggable: function(){
+		   	if(this.options.draggable == true){
+				  $(this.options.element_draggable).draggable({					  					
+				  revert: "invalid", 			
+				  helper: "clone"
+			  });		 	 
+		    }
+		   	return this.options.draggable;
+	    },
+	    
+	    _isDroppable: function(){
+			 if(this.options.droppable == true){
+				   $(this.options.element_droppable).droppable({     
+						  drop: function( event, ui ) {
+					      var elemento_dragg = ui.draggable.clone();
+					      $(this).append(elemento_dragg);
+						  $(elemento_dragg).removeAttr("style").removeClass('draggable');				
+					      }		
+					});
+			 }
+			 return this.options.droppable;
 	    }
  
 });
